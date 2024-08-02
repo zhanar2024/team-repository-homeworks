3e1acff797d093f0b429db0e37e7c519176833be
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-08-02 12:39:47 -0400

    Completed The Fortune teller: with functions!

---
+++ fortune-teller-function.js
@@ -0,0 +1,7 @@
+function tellFortune(numOfChildren, partnerName, geoLoc, jobTitle) {
+  console.log(
+    `You will be a ${jobTitle} in ${geoLoc}, and married to ${partnerName} with ${numOfChildren} kids.`
+  );
+}
+
+tellFortune(2, "Aibek", "Boston, MA", "Data Analyst");
